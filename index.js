2ce37a8f246324cd8f1474cd7993dcb0f742face
AyebkA <Ayebk123@gmail.com>
2022-03-05 13:49:07 +0200

    heroku

--- index.js
+++ index.js
@@ -23,11 +23,19 @@ mongoose
   });
 
 
-  let corsOptions = {
-    origin: ["https://ecommerce-food-production.herokuapp.com/"],
-  };
+  app.use((req, res, next) => {
 
-app.use(cors(corsOptions));
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept,Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+
+
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 
