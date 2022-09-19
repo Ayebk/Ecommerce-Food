2f97988cbc9d3b6531bffd43f363b5450d48bd94
Ayebk <Ayebk123@gmail.com>
2022-09-19 10:49:48 +0300

    Updating cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
   });
 
 
-app.use(cors());
+app.use(cors({ origin: "https://ecommerce-food-production.herokuapp.com", credentials: true }))
 app.use(express.json());
 app.use(morgan("dev"));
 
